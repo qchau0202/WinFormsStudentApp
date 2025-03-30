3341e296cb9f2820bd65b303cb7b08adc3da1238
iamchouty <quocchau4729@gmail.com>
2025-03-30 10:13:47 +0700

    Add project files.

---
+++ BAL/CourseService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BAL
+{
+    public class CourseService
+    {
+        private readonly CourseRepository _courseRepository;
+        public CourseService()
+        {
+            _courseRepository = new CourseRepository();
+        }
+        public List<tblCourses> GetAllCourses()
+        {
+            return _courseRepository.GetAllCourses();
+        }
+        public tblCourses AddCourse(tblCourses course)
+        {
+            return _courseRepository.AddCourse(course);
+        }
+        public tblCourses GetCourseById(int courseID)
+        {
+            return _courseRepository.GetCourseById(courseID);
+        }
+        public tblCourses UpdateCourse(tblCourses course)
+        {
+            return _courseRepository.UpdateCourse(course);
+        }
+        public bool DeleteCourse(int courseID)
+        {
+            return _courseRepository.DeleteCourse(courseID);
+        }
+    }
+}
---
+++ lab04/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="lab04.MvcApplication" Language="VB" %>
